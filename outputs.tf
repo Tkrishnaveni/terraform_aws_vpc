f5acb581ac7f36a9c6e144064a496cee4ce77774
Tkrishnaveni <kitu.krish10@gmail.com>
2025-02-16 12:31:07 +0530

    terraform vpc

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,15 @@ output "azs_info"{
 output "subnets_info"{
     value = aws_subnet.public
 }
+output "vpc_id"{
+    value = aws_vpc.main.id
+}
+output "public_subnet_ids"{
+    value =aws_sunnet.public[*].id
+}
+output "private_subnet_ids"{
+    value =aws_sunnet.private[*].id
+}
+output "database_subnet_ids"{
+    value =aws_sunnet.database[*].id
+}
