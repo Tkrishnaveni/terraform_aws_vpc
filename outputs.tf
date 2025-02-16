42e99257f50a1b609d9ee786065871228326e76e
Tkrishnaveni <kitu.krish10@gmail.com>
2025-02-16 21:27:25 +0530

    terraform_aws_vpc_module

--- outputs.tf
+++ outputs.tf
@@ -10,11 +10,11 @@ output "vpc_id"{
     value = aws_vpc.main.id
 }
 output "public_subnet_ids"{
-    value =aws_sunnet.public[*].id
+    value =aws_subnet.public[*].id
 }
 output "private_subnet_ids"{
-    value =aws_sunnet.private[*].id
+    value =aws_subnet.private[*].id
 }
 output "database_subnet_ids"{
-    value =aws_sunnet.database[*].id
+    value =aws_subnet.database[*].id
 }
